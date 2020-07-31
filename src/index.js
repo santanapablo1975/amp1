9526df332fb0fcefdec32e0d44b27c03944fbf2a
Juan S <santanapablo1975@gmail.com>
2020-07-31 00:11:03 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Checkout from './components/Checkout';
+import Success from './components/Success';
+import Canceled from './components/Canceled';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './css/normalize.css';
+import './css/global.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/canceled">
+          <Canceled />
+        </Route>
+        <Route path="/">
+          <Checkout />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
